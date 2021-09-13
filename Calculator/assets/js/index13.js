9e632d378093f2ee50851f50d27a01f1b877eec4
Gustavo Kurek <gustavokureksz@outlook.com>
2021-09-13 19:32:08 -0300

    commit add calculator

---
+++ Calculator/assets/js/index13.js
@@ -0,0 +1,83 @@
+function getCalculator(){
+    return{
+        
+        display: document.querySelector('.display'),
+
+
+    Start(){
+    this.clickBtn();
+    this.clickEnter();
+    },
+
+
+     clickEnter(){
+         document.addEventListener('keyup', (e)=>{
+             if (e.keyCode===13){
+            this.calc()
+
+             }    
+         })},
+
+    clear(){
+        this.display.value=''
+    },   
+    
+    clearOne(){
+        this.display.value= this.display.value.slice(0, -1);
+    },
+
+    calc(){
+        let conta=this.display.value;
+
+         try{
+             conta=eval(conta);
+             if(!conta){
+                 alert('conta inválida')
+                 return
+             }
+
+             this.display.value=String(conta);
+         } catch(e){
+             alert('conta inválida')
+
+         }
+    },
+
+
+
+     clickBtn(){
+         document.addEventListener('click', (e)=>{
+             const el=e.target;
+
+             if(el.classList.contains('btn-num')){
+             this.btnForDisplay(el.innerText)};
+
+             if(el.classList.contains('btn-clear')){
+                this.clear()
+             }
+             if(el.classList.contains('btn-del')){
+                this.clearOne()
+             }             
+             if(el.classList.contains('btn-eq')){
+                this.calc()
+             }
+
+            this.display.focus();
+
+        
+         });
+        },
+ 
+        btnForDisplay(valor){
+            this.display.value+=valor
+
+        }
+    
+        
+
+    
+    
+}}
+
+const calculator=getCalculator()
+calculator.Start();
