2c2409f1260a9f900bc3a983421e0f712267163e
Gustavo Kurek <gustavokureksz@outlook.com>
2021-09-14 14:25:40 -0300

    commit

--- Calculator/assets/js/index13.js
+++ Calculator/assets/js/index13.js
@@ -9,17 +9,47 @@ function getCalculator(){
     this.clickEnter();
     },
 
+     clickBtn(){
+         document.addEventListener('click', (e)=>{
+             const el=e.target;
 
+             if(el.classList.contains('btn-num')){
+             this.btnForDisplay(el.innerText)};
+
+             if(el.classList.contains('btn-clear')){
+                this.clear();
+             }
+             if(el.classList.contains('btn-del')){
+                this.clearOne();
+             }             
+             if(el.classList.contains('btn-eq')){
+                this.calc();
+             }
+
+            this.display.focus();
+
+        
+         });
+        },
+ 
      clickEnter(){
-         document.addEventListener('keyup', (e)=>{
-             if (e.keyCode===13){
-            this.calc()
+         document.addEventListener('keyup', function(e){ //usando factory function para o this copntinuar a calculadora e não mudar para document, altetiva no fim da função usar .
 
-             }    
+             if (e.keyCode===13){
+            this.calc();
+
+             };    
          })},
 
+
+
+    btnForDisplay(valor){
+        this.display.value+=valor;
+
+    },
+
     clear(){
-        this.display.value=''
+        this.display.value='';
     },   
     
     clearOne(){
@@ -43,39 +73,6 @@ function getCalculator(){
          }
     },
 
-
-
-     clickBtn(){
-         document.addEventListener('click', (e)=>{
-             const el=e.target;
-
-             if(el.classList.contains('btn-num')){
-             this.btnForDisplay(el.innerText)};
-
-             if(el.classList.contains('btn-clear')){
-                this.clear()
-             }
-             if(el.classList.contains('btn-del')){
-                this.clearOne()
-             }             
-             if(el.classList.contains('btn-eq')){
-                this.calc()
-             }
-
-            this.display.focus();
-
-        
-         });
-        },
- 
-        btnForDisplay(valor){
-            this.display.value+=valor
-
-        }
-    
-        
-
-    
     
 }}
 
